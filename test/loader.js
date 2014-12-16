197903a95c747ef02d2d4e4e07780e580a611562
skinnybrit51 <skinnybrit51@gmail.com>
2014-12-15 22:31:48 -0500

    return the view configuration

--- test/loader.js
+++ test/loader.js
@@ -1,5 +1,16 @@
-var jsdom = require('jsdom');
+var fs = require('fs'),
+    jsdom = require('jsdom'),
+    haml = require('haml');
 
 /* Create the dom */
 global.document = global.document || jsdom.jsdom();
 global.window = global.window || global.document.createWindow();
+
+require('jquery');
+
+/* Compile haml extensions on the fly */
+require.extensions['.haml'] = function (module, filename) {
+    var contents = fs.readFileSync(filename).toString();
+    var compiled = 'module.exports = ' + haml(contents).toString();
+    return module._compile(compiled);
+};
