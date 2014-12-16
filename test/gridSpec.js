ceb36a3594121d2d8ab4a5c838f54f953e58d955
skinnybrit51 <skinnybrit51@gmail.com>
2014-12-16 13:42:09 -0500

    implement simple sort

--- test/gridSpec.js
+++ test/gridSpec.js
@@ -7,8 +7,9 @@ var _ = require('underscore'),
 
 describe('Grid', function () {
 
-    beforeEach(function () {
-        this.columns = [
+    it('Should return the grid view', function () {
+
+        var columns = [
             {
                 id: 'col-1',
                 width: 100,
@@ -25,21 +26,14 @@ describe('Grid', function () {
                 title: 'boo'
             }
         ];
-        this.grid = new Grid({
+        var grid = new Grid({
             el: $('<div/>'),
-            columns: this.columns,
+            columns: columns,
             data: []
         });
-        this.grid.render();
-    });
+        grid.render();
 
-    afterEach(function () {
-        delete this.grid;
-    });
-
-    it('Should return the grid view', function () {
-
-        var view = this.grid.getView();
+        var view = grid.getView();
         expect(view.columns).to.have.length(3);
 
         expect(_.keys(view.columns[0])).to.have.length(2);
@@ -53,5 +47,69 @@ describe('Grid', function () {
         expect(_.keys(view.columns[0])).to.have.length(2);
         expect(view.columns[2].id).to.equal('col-c');
         expect(view.columns[2].width).to.equal(500);
+    });
+
+    it('Should sort the column', function () {
+        var container = $('<div/>');
+        var columns = [
+            {
+                id: 'col-1',
+                width: 100,
+                title: 'boo'
+            }
+        ];
+        var grid = new Grid({
+            el: container,
+            columns: columns,
+            data: [
+                {
+                    id: '1',
+                    'col-1': 'b'
+                },
+                {
+                    id: '2',
+                    'col-1': 'a'
+                },
+                {
+                    id: '3',
+                    'col-1': 'c'
+                }
+            ]
+        });
+        grid.render();
+
+        var th = container.find('th');
+        expect(th.attr('data-property-name')).to.equal('col-1');
+
+        // note the current order
+        var td = container.find('tbody td:not([data-property-name="empty-last-column"])');
+        expect(td).to.have.length(3);
+        expect(td.eq(0).text()).to.equal('b');
+        expect(td.eq(1).text()).to.equal('a');
+        expect(td.eq(2).text()).to.equal('c');
+
+        // ascending order
+        th.trigger('click');   // simulate header click
+        td = container.find('tbody td:not([data-property-name="empty-last-column"])');
+        expect(td).to.have.length(3);
+        expect(td.eq(0).text()).to.equal('a');
+        expect(td.eq(1).text()).to.equal('b');
+        expect(td.eq(2).text()).to.equal('c');
+
+        // descending order
+        container.find('th').trigger('click');   // simulate header click
+        td = container.find('tbody td:not([data-property-name="empty-last-column"])');
+        expect(td).to.have.length(3);
+        expect(td.eq(0).text()).to.equal('c');
+        expect(td.eq(1).text()).to.equal('b');
+        expect(td.eq(2).text()).to.equal('a');
+
+        // original order
+        container.find('th').trigger('click');   // simulate header click
+        td = container.find('tbody td:not([data-property-name="empty-last-column"])');
+        expect(td).to.have.length(3);
+        expect(td.eq(0).text()).to.equal('b');
+        expect(td.eq(1).text()).to.equal('a');
+        expect(td.eq(2).text()).to.equal('c');
     });
 });
