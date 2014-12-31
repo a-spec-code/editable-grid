f7ab13142f83ac952235f08e693a8cfc6fd0ecac
skinnybrit51 <skinnybrit51@gmail.com>
2014-12-31 13:24:42 +0000

    more tests for record selection

--- test/selectionModelSpec.js
+++ test/selectionModelSpec.js
@@ -1,5 +1,4 @@
-var _ = require('underscore'),
-    expect = require('chai').expect,
+var expect = require('chai').expect,
     selectionModel = require('selectionModel'),
     StateManager = require('stateManager'),
     Ears = require('elephant-ears'),
@@ -237,14 +236,15 @@ describe('Selection Model', function () {
                 },
                 removeClass: function () {
                 }
-            }
+            };
         });
 
         this.selectionModel.select(0, 0);
 
         expect(findSpy.callCount).to.equal(2);
         expect(findSpy.args[0][0]).to.equal('.selected');
-        expect(findSpy.args[1][0]).to.equal('tr[data-record-id="1"] td[data-property-name="col-1"]');
+        expect(findSpy.args[1][0])
+            .to.equal('tr[data-record-id="1"] td[data-property-name="col-1"]');
 
         expect(this.view.recordTopIndex).to.equal(0);
         expect(this.view.columnTopIndex).to.equal(0);
@@ -277,14 +277,14 @@ describe('Selection Model', function () {
                 },
                 removeClass: function () {
                 }
-            }
+            };
         });
         var cssValues = {
             bottom: null
         };
         var cssSpy = this.sandbox.stub(this.view.tableBody, 'css', function (key, value) {
             if (value == null) {
-                if ((cssValues[key] == null || cssValues[key] === "") && key === 'bottom') {
+                if ((cssValues[key] == null || cssValues[key] === '') && key === 'bottom') {
                     return '0px';   // default value
                 }
                 return cssValues[key];
@@ -588,6 +588,225 @@ describe('Selection Model', function () {
         expect(cssSpy.args[49][1]).to.equal(undefined);
         expect(this.view.recordTopIndex).to.equal(0);
         expect(renderSpy.callCount).to.equal(20);
+
+    });
+
+    it('Should select the correct cell for a 250 height', function () {
+        this.view.TBODY_HEIGHT = 220;
+
+        this.view.recordTopIndex = 0;
+        this.view.columnTopIndex = 0;
+        this.view.columnBottomIndex = 1;
+
+        var data = [];
+        for (var r = 0; r < 12; r++) {
+            data.push({
+                id: r
+            });
+        }
+        var renderSpy = this.sandbox.spy(this.grid, 'render');
+
+        this.sandbox.stub(this.grid._stateManager, 'getRecords', function () {
+            return data;
+        });
+
+        this.sandbox.stub(this.view.tbody, 'find', function () {
+            return {
+                addClass: function () {
+                },
+                removeClass: function () {
+                }
+            };
+        });
+        var cssValues = {
+            bottom: null
+        };
+        var cssSpy = this.sandbox.stub(this.view.tableBody, 'css', function (key, value) {
+            if (value == null) {
+                if ((cssValues[key] == null || cssValues[key] === '') && key === 'bottom') {
+                    return '0px';   // default value
+                }
+                return cssValues[key];
+            }
+            cssValues[key] = value;
+        });
+
+        // MOVING DOWN BY ONE SPACE AT A TIME
+
+        expect(cssSpy.callCount).to.equal(0);
+        expect(this.view.recordTopIndex).to.equal(0);
+
+        this.selectionModel.select(1, 0);
+        expect(cssSpy.callCount).to.equal(1);
+        expect(cssSpy.args[0][0]).to.equal('bottom');
+        expect(cssSpy.args[0][1]).to.equal(undefined);
+        expect(this.view.recordTopIndex).to.equal(0);
+        expect(renderSpy.callCount).to.equal(0);
+
+        this.selectionModel.select(2, 0);
+        expect(cssSpy.callCount).to.equal(2);
+        expect(cssSpy.args[1][0]).to.equal('bottom');
+        expect(cssSpy.args[1][1]).to.equal(undefined);
+        expect(this.view.recordTopIndex).to.equal(0);
+        expect(renderSpy.callCount).to.equal(0);
+
+        this.selectionModel.select(3, 0);
+        expect(cssSpy.callCount).to.equal(3);
+        expect(cssSpy.args[2][0]).to.equal('bottom');
+        expect(cssSpy.args[2][1]).to.equal(undefined);
+        expect(this.view.recordTopIndex).to.equal(0);
+        expect(renderSpy.callCount).to.equal(0);
+
+        this.selectionModel.select(4, 0);
+        expect(cssSpy.callCount).to.equal(4);
+        expect(cssSpy.args[3][0]).to.equal('bottom');
+        expect(cssSpy.args[3][1]).to.equal(undefined);
+        expect(this.view.recordTopIndex).to.equal(0);
+        expect(renderSpy.callCount).to.equal(0);
+
+        this.selectionModel.select(5, 0);
+        expect(cssSpy.callCount).to.equal(5);
+        expect(cssSpy.args[4][0]).to.equal('bottom');
+        expect(cssSpy.args[4][1]).to.equal(undefined);
+        expect(this.view.recordTopIndex).to.equal(0);
+        expect(renderSpy.callCount).to.equal(0);
+
+        this.selectionModel.select(6, 0);
+        expect(cssSpy.callCount).to.equal(7);
+        expect(cssSpy.args[5][0]).to.equal('bottom');
+        expect(cssSpy.args[5][1]).to.equal(undefined);
+        expect(cssSpy.args[6][0]).to.equal('bottom');
+        expect(cssSpy.args[6][1]).to.equal('');
+        expect(this.view.recordTopIndex).to.equal(1);
+        expect(renderSpy.callCount).to.equal(1);
+
+        this.selectionModel.select(7, 0);
+        expect(cssSpy.callCount).to.equal(9);
+        expect(cssSpy.args[7][0]).to.equal('bottom');
+        expect(cssSpy.args[7][1]).to.equal(undefined);
+        expect(cssSpy.args[8][0]).to.equal('bottom');
+        expect(cssSpy.args[8][1]).to.equal('');
+        expect(this.view.recordTopIndex).to.equal(2);
+        expect(renderSpy.callCount).to.equal(2);
+
+        this.selectionModel.select(8, 0);
+        expect(cssSpy.callCount).to.equal(11);
+        expect(cssSpy.args[9][0]).to.equal('bottom');
+        expect(cssSpy.args[9][1]).to.equal(undefined);
+        expect(cssSpy.args[10][0]).to.equal('bottom');
+        expect(cssSpy.args[10][1]).to.equal('');
+        expect(this.view.recordTopIndex).to.equal(3);
+        expect(renderSpy.callCount).to.equal(3);
+
+        this.selectionModel.select(9, 0);
+        expect(cssSpy.callCount).to.equal(13);
+        expect(cssSpy.args[11][0]).to.equal('bottom');
+        expect(cssSpy.args[11][1]).to.equal(undefined);
+        expect(cssSpy.args[12][0]).to.equal('bottom');
+        expect(cssSpy.args[12][1]).to.equal('');
+        expect(this.view.recordTopIndex).to.equal(4);
+        expect(renderSpy.callCount).to.equal(4);
+
+        this.selectionModel.select(10, 0);
+        expect(cssSpy.callCount).to.equal(15);
+        expect(cssSpy.args[13][0]).to.equal('bottom');
+        expect(cssSpy.args[13][1]).to.equal(undefined);
+        expect(cssSpy.args[14][0]).to.equal('bottom');
+        expect(cssSpy.args[14][1]).to.equal('7px');
+        expect(this.view.recordTopIndex).to.equal(4);
+        expect(renderSpy.callCount).to.equal(4);
+
+        this.selectionModel.select(11, 0);
+        expect(cssSpy.callCount).to.equal(17);
+        expect(cssSpy.args[15][0]).to.equal('bottom');
+        expect(cssSpy.args[15][1]).to.equal(undefined);
+        expect(cssSpy.args[16][0]).to.equal('bottom');
+        expect(cssSpy.args[16][1]).to.equal('37px');
+        expect(this.view.recordTopIndex).to.equal(4);
+        expect(renderSpy.callCount).to.equal(4);
+
+        // NOW MOVE UP
+
+        this.selectionModel.select(10, 0);
+        expect(cssSpy.callCount).to.equal(19);
+        expect(cssSpy.args[17][0]).to.equal('bottom');
+        expect(cssSpy.args[17][1]).to.equal(undefined);
+        expect(cssSpy.args[18][0]).to.equal('bottom');
+        expect(cssSpy.args[18][1]).to.equal('7px');
+        expect(this.view.recordTopIndex).to.equal(4);
+        expect(renderSpy.callCount).to.equal(4);
+
+        this.selectionModel.select(9, 0);
+        expect(cssSpy.callCount).to.equal(20);
+        expect(cssSpy.args[19][0]).to.equal('bottom');
+        expect(cssSpy.args[19][1]).to.equal(undefined);
+        expect(this.view.recordTopIndex).to.equal(4);
+        expect(renderSpy.callCount).to.equal(4);
+
+        this.selectionModel.select(8, 0);
+        expect(cssSpy.callCount).to.equal(21);
+        expect(cssSpy.args[20][0]).to.equal('bottom');
+        expect(cssSpy.args[20][1]).to.equal(undefined);
+        expect(this.view.recordTopIndex).to.equal(4);
+        expect(renderSpy.callCount).to.equal(4);
+
+        this.selectionModel.select(7, 0);
+        expect(cssSpy.callCount).to.equal(22);
+        expect(cssSpy.args[21][0]).to.equal('bottom');
+        expect(cssSpy.args[21][1]).to.equal(undefined);
+        expect(this.view.recordTopIndex).to.equal(4);
+        expect(renderSpy.callCount).to.equal(4);
+
+        this.selectionModel.select(6, 0);
+        expect(cssSpy.callCount).to.equal(23);
+        expect(cssSpy.args[22][0]).to.equal('bottom');
+        expect(cssSpy.args[22][1]).to.equal(undefined);
+        expect(this.view.recordTopIndex).to.equal(4);
+        expect(renderSpy.callCount).to.equal(4);
+
+        this.selectionModel.select(5, 0);
+        expect(cssSpy.callCount).to.equal(24);
+        expect(cssSpy.args[23][0]).to.equal('bottom');
+        expect(cssSpy.args[23][1]).to.equal(undefined);
+        expect(this.view.recordTopIndex).to.equal(4);
+        expect(renderSpy.callCount).to.equal(4);
+
+        this.selectionModel.select(4, 0);
+        expect(cssSpy.callCount).to.equal(26);
+        expect(cssSpy.args[24][0]).to.equal('bottom');
+        expect(cssSpy.args[24][1]).to.equal(undefined);
+        expect(cssSpy.args[25][0]).to.equal('bottom');
+        expect(cssSpy.args[25][1]).to.equal('');
+        expect(this.view.recordTopIndex).to.equal(4);
+        expect(renderSpy.callCount).to.equal(4);
+
+        this.selectionModel.select(3, 0);
+        expect(cssSpy.callCount).to.equal(27);
+        expect(cssSpy.args[26][0]).to.equal('bottom');
+        expect(cssSpy.args[26][1]).to.equal(undefined);
+        expect(this.view.recordTopIndex).to.equal(3);
+        expect(renderSpy.callCount).to.equal(5);
+
+        this.selectionModel.select(2, 0);
+        expect(cssSpy.callCount).to.equal(28);
+        expect(cssSpy.args[27][0]).to.equal('bottom');
+        expect(cssSpy.args[27][1]).to.equal(undefined);
+        expect(this.view.recordTopIndex).to.equal(2);
+        expect(renderSpy.callCount).to.equal(6);
+
+        this.selectionModel.select(1, 0);
+        expect(cssSpy.callCount).to.equal(29);
+        expect(cssSpy.args[28][0]).to.equal('bottom');
+        expect(cssSpy.args[28][1]).to.equal(undefined);
+        expect(this.view.recordTopIndex).to.equal(1);
+        expect(renderSpy.callCount).to.equal(7);
+
+        this.selectionModel.select(0, 0);
+        expect(cssSpy.callCount).to.equal(30);
+        expect(cssSpy.args[29][0]).to.equal('bottom');
+        expect(cssSpy.args[29][1]).to.equal(undefined);
+        expect(this.view.recordTopIndex).to.equal(0);
+        expect(renderSpy.callCount).to.equal(8);
 
     });
 });
